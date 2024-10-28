6cfaf38142e16bdfad7ddc3c0f78ba3560787348
aissa hadjoudja <aissa.hajdoudja@akka.eu>
2024-10-28 16:56:45 +0100

    Refactored metrics to consider target (either fs or s3)

--- wis2downloader/metrics.py
+++ wis2downloader/metrics.py
@@ -3,15 +3,18 @@ from prometheus_client import Counter, Gauge
 
 QUEUE_SIZE = Gauge(
     'queue_size', 'Current size of the job queue')
-DOWNLOADED_BYTES = Counter(
-    'downloaded_bytes', 'Total number of downloaded bytes',
-    ['topic', 'centre_id', 'file_type'])
-DOWNLOADED_FILES = Counter(
-    'downloaded_files', 'Total number of downloaded files',
-    ['topic', 'centre_id', 'file_type'])
-FAILED_DOWNLOADS = Counter(
-    'failed_downloads', 'Total number of failed downloads',
-    ['topic', 'centre_id'])
 TOPIC_STATUS = Gauge(
     'topic_subscription_status', 'Subscription status of a given topic',
     ['topic'])
+DOWNLOADED_BYTES = Counter(
+    'downloaded_bytes', 'Total number of downloaded bytes',
+    ['target', 'topic', 'centre_id', 'file_type'])
+DOWNLOADED_FILES = Counter(
+    'downloaded_files', 'Total number of downloaded files',
+    ['target', 'topic', 'centre_id', 'file_type'])
+FAILED_DOWNLOADS = Counter(
+    'failed_downloads', 'Total number of failed downloads to either fs o s3',
+    ['target', 'topic', 'centre_id'])
+FAILED_DOWNLOADS_BROKER = Counter(
+    'failed_downloads', 'Total number of failed downloads from broker ',
+    ['topic', 'centre_id'])
